56bb5c7c4475c626c93d124d6637539efce3ccbf
fengwt <vincent.feng@fwd.com>
2019-10-15 11:39:39 +0800

    Initial commit

---
+++ src/main/java/model/Claim.java
@@ -0,0 +1,23 @@
+package model;
+import java.math.BigDecimal;
+
+public class Claim {
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    String no;
+    BigDecimal amount;
+}
